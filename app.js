b5c0c318c5e2550d671ec7f3e1ac33f0cef08d65
ewrosky <98802240+ewrosky@users.noreply.github.com>
2024-11-06 00:03:43 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch(error => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.static('dist'))
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
