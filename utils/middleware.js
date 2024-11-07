6f525a7caddac56f0e6221aa4389dfef60f0513e
ewrosky <98802240+ewrosky@users.noreply.github.com>
2024-11-07 06:15:08 -0300

    Blog List Expansion 4.15-4.23

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,5 +1,8 @@
 const logger = require('./logger')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+const config = require('../utils/config')
+
 
 const requestLogger = (request, response, next) => {
   logger.info('Method:', request.method)
@@ -42,7 +45,7 @@ const tokenExtractor = (request, response, next) => {
 }
 
 const userExtractor = async (request, response, next) => {
-  const decodedToken = json.verify(request.token, config.SECRET)
+  const decodedToken = jwt.verify(request.token, config.SECRET)
   if (!decodedToken.id) {
     return response.status(401).json({ error: 'token invalid' })
   }
