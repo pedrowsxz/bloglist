5b1e67a3ff379f2ffca69ff2f4d4bfcd07558505
ewrosky <98802240+ewrosky@users.noreply.github.com>
2024-11-08 06:24:08 -0300

    Minor adjustments

--- utils/middleware.js
+++ utils/middleware.js
@@ -38,16 +38,17 @@ const errorHandler = (error, request, response, next) => {
 const tokenExtractor = (request, response, next) => {
   const authorization = request.get('authorization')
   if (authorization && authorization.startsWith('Bearer')) {
-    request.token = authorization.replace('Bearer', '')
+    request.token = authorization.replace('Bearer ', '')
+  } else {
+    request.token = null
   }
-  request.token = null
   next()
 }
 
 const userExtractor = async (request, response, next) => {
   const decodedToken = jwt.verify(request.token, config.SECRET)
   if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
+    return response.json({ error: 'token invalid' }).status(401)
   }
   const user = await User.findById(decodedToken.id)
   request.user = user
